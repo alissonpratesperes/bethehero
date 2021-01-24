e5fcfed28660969285be134f5a3877d14b3872b4
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-24 20:29:54 -0300

    Criação das Rotas INDEX, CREATE, DELETE para o "IncidentController".
    Criado o "IncidentController.js" com os métodos de LISTAGEM, CRIAÇÃO e DELEÇÃO dos Casos;
    Criadas as Rotas novas para cada método do "IncidentController.js";

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -2,7 +2,6 @@ const crypto = require('crypto');
 const connection = require('../database/connection');
 
     module.exports = {
-
         async index(request, response) {
             const ongs = await connection('ongs').select('*');
                 return response.json(ongs);
@@ -10,14 +9,7 @@ const connection = require('../database/connection');
         async create(request, response) {
             const { name, email, whatsapp, city, uf } = request.body;
             const id = crypto.randomBytes(4).toString('HEX');
-                await connection('ongs').insert({
-                    id,
-                    name,
-                    email,
-                    whatsapp,
-                    city,
-                    uf
-                });
+                await connection('ongs').insert({ id, name, email, whatsapp, city, uf });
                     return response.json({ id });
         }
     };
