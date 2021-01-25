cf673c95174745d8f256f6a3200da58eadd543e8
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-24 21:36:02 -0300

    Organização geral no Código-Fonte.
    Organização geral e limpeza no Código-Fonte do BACKEND.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const OngController = require('./controllers/OngController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
-    routes.post('/sessions', SessionController.create);
-    routes.get('/ongs', OngController.index);
-    routes.post('/ongs', OngController.create);
-    routes.get('/profile', ProfileController.index);
     routes.get('/incidents', IncidentController.index);
     routes.post('/incidents', IncidentController.create);
     routes.delete('/incidents/:id', IncidentController.delete);
-
+    routes.get('/ongs', OngController.index);
+    routes.post('/ongs', OngController.create); 
+    routes.get('/profile', ProfileController.index); 
+    routes.post('/sessions', SessionController.create);
+    
         module.exports = routes;
