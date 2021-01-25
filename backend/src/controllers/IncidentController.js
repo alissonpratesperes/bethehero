3f8420224df93a8b9346b38e098b3fb36a975d99
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-24 21:19:15 -0300

    Relacionamento entre ONG x CASO.
    Implementado o "relacionamento ONG x CASO" onde agora na LISTAGEM DOS CASOS, aparecerão os DADOS DA ONG que criou cada CASO.

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -4,7 +4,7 @@ const connection = require('../database/connection');
         async index(request, response) {
             const { page = 1 } = request.query;
             const [count] = await connection('incidents').count();
-            const incidents = await connection('incidents').limit(5).offset((page - 1) * 5).select('*');
+            const incidents = await connection('incidents').join('ongs', 'ongs.id', '=', 'incidents.ong_id').limit(5).offset((page - 1) * 5).select([ 'incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf' ]);
                 response.header('X-Total-Count', count['count(*)']);
                     return response.json(incidents);
         },
