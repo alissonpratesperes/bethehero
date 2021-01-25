0f56fd7e536901276e573f45927c9087a8e245f9
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-25 20:24:53 -0300

    Importação dos Assets do FrontEnd e CSS Global Inicial.
    Criada a estrutura inicial das Páginas da Aplicação;
    Importados os Assets do FRONTEND disponibilizados na Aula;
    Excluídos alguns arquivos de testes;
    Criado o arquivo de Estilização Global com o Reset Inicial Global dos estilos do documento.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Header from './Header';
+import './global.css';
+import Logon from './pages/Logon';
 
   function App() {
     return (
-      <Header title="Semana OmniStack"/>
+      <Logon />
     );
   }
 
