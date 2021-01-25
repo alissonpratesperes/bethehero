48d74556cbfd2f5b6695c9a76b10fe1d1a0b1767
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-24 22:00:16 -0300

    Limpeza da estrutura de arquivos inicial.
    Limpeza de alguns arquivos que não serão utilizados durante o desenvolvimento dessa aplicação, que foram criados pelo "create-react-app".

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <Header title="Semana OmniStack"/>
+    );
+  }
 
-export default App;
+    export default App;
