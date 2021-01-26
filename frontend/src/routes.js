34cb8b2c9cdd905b5eaebabdf1c13e105a37916e
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-25 21:40:14 -0300

    Instalação do React Router Dom e início do Roteamento das Páginas.
    Instalado então o React Router Dom para o roteamento das páginas da Aplicação;
    Criado o arquivo de Rotas com a primeira Rota configurada "/" e importado esse "routes.js" dentro do "App.js".

---
+++ frontend/src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Logon from './pages/Logon';
+
+    export default function Routes() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" component={Logon}/>
+                </Switch>
+            </BrowserRouter>
+        );
+    }
