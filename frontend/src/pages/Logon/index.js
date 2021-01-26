69a236b722490dbad21483b4702c2e6d54405b28
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-25 21:21:16 -0300

    Finalização da Página de Logon.
    Finalizada a Página de Logon com os Estilos CSS Internos iniciais dessa página conforme a aula;
    Inseridos alguns Estilos CSS Globais também conforme as instruções;
    Imputados alguns Estilos CSS prórpios visando a minha própria versão Front-End dessa Aplicação.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -12,8 +12,8 @@ import heroesImg from '../../assets/heroes.png';
                         <form>
                             <h1> Faça seu logon </h1>
                                 <input placeholder="Sua ID"/>
-                                    <button tyoe="submit"> Entrar </button>
-                                        <a href="/register"> <FiLogIn size={16} color="#E02041"/> Não tenho cadastro </a>
+                                    <button className="button" tyoe="submit"> Entrar </button>
+                                        <a href="/register"> <FiLogIn size={20} color="#E02041"/> Não tenho cadastro </a>
                         </form>
                 </section>
                     <img src={heroesImg} alt="Heroes"/>
