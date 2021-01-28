7bea826e4d9fa688366afeea5bf40dd9f1f4202a
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-27 22:08:48 -0300

    Ajustes gerais de CSS.
    Realizados alguns ajustes gerais de CSS nas duas páginas implementadas até agora.

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -10,9 +10,9 @@ import logoImg from '../../assets/logo.svg';
                 <div className="content">
                     <section>
                         <img src={logoImg} alt="Be The Hero"/>
-                            <h1> Cadastro </h1>
+                            <h1> Cadastre-se </h1>
                                 <p> Faça seu cadastro, entre na plataforma e ajude pesssoas a encontrarem os casos da sua ONG. </p>
-                                    <Link className="back-link" to="/"> <FiArrowLeft size={20} color="#E02041"/> Já tenho cadastro </Link>    
+                                    <Link className="back-link" to="/"> <FiArrowLeft size={20} color="#E02041"/> Já sou cadastrado </Link>    
                     </section>
                         <form>
                             <input placeholder="Nome da ONG"/>
