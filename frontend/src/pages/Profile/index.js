53e16e07b1457e38537d160a11ef5403c76e2bc3
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-28 22:07:24 -0300

    Inicialização do Header(Cabeçalho) da Página Profile.
    Inicializada a Estrutura HTML do Header(Cabeçalho) da Página Profile.

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 
     export default function Profile() {
-        return <h1> Profile </h1>
+        return (
+            <div className="profile-container">
+                <header>
+                    <img src={logoImg} alt="Be The Hero"/>
+                        <span> Bem-vinda, APAD </span>
+                            <Link className="button" to="/incidents/new"> Cadastrar Novo Caso </Link>
+                                <button type="button"> <FiPower  size={20} color="#E02041"/> </button>
+                </header>
+            </div>
+        );
     }
