49ae33d66efc2977bb312dce1638e2bc2c71d8df
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-30 00:34:13 -0300

    Estilização inicial da Página Profile - Body.
    Estilizado inicialmente, de acordo com o treinamento, o corpo da página Profile com todos os Estilos CSS referentes.

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
@@ -13,6 +13,45 @@ import logoImg from '../../assets/logo.svg';
                             <Link className="button" to="/incidents/new"> Cadastrar Novo Caso </Link>
                                 <button type="button"> <FiPower  size={25} color="#E02041"/> </button>
                 </header>
+                    <h1> Casos Cadastrados </h1>
+                        <ul>
+                            <li>
+                                <strong> CASO: </strong>
+                                    <p> Caso Teste </p>
+                                <strong> DESCRIÇÃO: </strong>
+                                    <p> Descrição Teste </p>
+                                <strong> VALOR: </strong>
+                                    <p> R$ 120,00 </p>
+                                        <button type="button"> <FiTrash2 size={20} color="#A8A8B3"/> </button>
+                            </li>
+                            <li>
+                                <strong> CASO: </strong>
+                                    <p> Caso Teste </p>
+                                <strong> DESCRIÇÃO: </strong>
+                                    <p> Descrição Teste </p>
+                                <strong> VALOR: </strong>
+                                    <p> R$ 120,00 </p>
+                                        <button type="button"> <FiTrash2 size={20} color="#A8A8B3"/> </button>
+                            </li>
+                            <li>
+                                <strong> CASO: </strong>
+                                    <p> Caso Teste </p>
+                                <strong> DESCRIÇÃO: </strong>
+                                    <p> Descrição Teste </p>
+                                <strong> VALOR: </strong>
+                                    <p> R$ 120,00 </p>
+                                        <button type="button"> <FiTrash2 size={20} color="#A8A8B3"/> </button>
+                            </li>
+                            <li>
+                                <strong> CASO: </strong>
+                                    <p> Caso Teste </p>
+                                <strong> DESCRIÇÃO: </strong>
+                                    <p> Descrição Teste </p>
+                                <strong> VALOR: </strong>
+                                    <p> R$ 120,00 </p>
+                                        <button type="button"> <FiTrash2 size={20} color="#A8A8B3"/> </button>
+                            </li>
+                        </ul>
             </div>
         );
     }
