b3a84474b8c0e91ddad5e3a3ae0a6e8344e9ee3f
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-31 20:57:04 -0300

    LocalStorage + GET na API do BACKEND na Página Profile para a ONG logada.
    Utilizado o recurso do "LocalStorage" para já coletar qual ONG logada e substituir no Header da Página Profile;
    Implementado o React Hook "useEffect" para pegar os "Headers - Authorization" do GET na API pra listar os Casos da ONG logada;
    Apagados os Casos estáticos feitos durante o desenvolvimento do Front-End e implementada a Iteração com somente um dos List Items, realizando um map e listando, de acordo com a ONG cadastrada, o total de casos para ela.

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,57 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+import api from '../../services/api';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
     export default function Profile() {
-        return (
-            <div className="profile-container">
-                <header>
-                    <img src={logoImg} alt="Be The Hero"/>
-                        <span> Bem-vinda, APAD </span>
-                            <Link className="button" to="/incidents/new"> Cadastrar Caso </Link>
-                                <button type="button"> <FiPower  size={25} color="#E02041"/> </button>
-                </header>
-                    <h1> Casos Cadastrados </h1>
-                        <ul>
-                            <li>
-                                <strong> CASO: </strong>
-                                    <p> Caso Teste </p>
-                                <strong> DESCRIÇÃO: </strong>
-                                    <p> Descrição Teste </p>
-                                <strong> VALOR: </strong>
-                                    <p> R$ 120,00 </p>
-                                        <button type="button"> <FiTrash2 size={25} color="#E02041"/> </button>
-                            </li>
-                            <li>
-                                <strong> CASO: </strong>
-                                    <p> Caso Teste </p>
-                                <strong> DESCRIÇÃO: </strong>
-                                    <p> Descrição Teste </p>
-                                <strong> VALOR: </strong>
-                                    <p> R$ 120,00 </p>
-                                        <button type="button"> <FiTrash2 size={25} color="#E02041"/> </button>
-                            </li>
-                            <li>
-                                <strong> CASO: </strong>
-                                    <p> Caso Teste </p>
-                                <strong> DESCRIÇÃO: </strong>
-                                    <p> Descrição Teste </p>
-                                <strong> VALOR: </strong>
-                                    <p> R$ 120,00 </p>
-                                        <button type="button"> <FiTrash2 size={25} color="#E02041"/> </button>
-                            </li>
-                            <li>
-                                <strong> CASO: </strong>
-                                    <p> Caso Teste </p>
-                                <strong> DESCRIÇÃO: </strong>
-                                    <p> Descrição Teste </p>
-                                <strong> VALOR: </strong>
-                                    <p> R$ 120,00 </p>
-                                        <button type="button"> <FiTrash2 size={25} color="#E02041"/> </button>
-                            </li>
-                        </ul>
-            </div>
-        );
+        const [incidents, setIncidents] = useState([]);
+        const ongId = localStorage.getItem('ongId');
+        const ongName = localStorage.getItem('ongName');
+
+            useEffect(() => {
+                api.get('profile', { 
+                    headers: { Authorization: ongId } }).then(response => { setIncidents(response.data); });
+            }, [ongId]);
+
+                return (
+                    <div className="profile-container">
+                        <header>
+                            <img src={logoImg} alt="Be The Hero"/>
+                                <span> Bem-vinda, {ongName} </span>
+                                    <Link className="button" to="/incidents/new"> Cadastrar Caso </Link>
+                                        <button type="button"> <FiPower  size={25} color="#E02041"/> </button>
+                        </header>
+                            <h1> Casos Cadastrados </h1>
+                                <ul>
+                                    {incidents.map(incident => (
+                                        <li key={incident.id}>
+                                            <strong> CASO: </strong>
+                                                <p> {incident.title} </p>
+                                            <strong> DESCRIÇÃO: </strong>
+                                                <p> {incident.description} </p>
+                                            <strong> VALOR: </strong>
+                                                <p> {incident.value} </p>
+                                                    <button type="button"> <FiTrash2 size={25} color="#E02041"/> </button>
+                                        </li>   
+                                    ))}                  
+                                </ul>
+                    </div>
+                );
     }
