c777cbdb9a982e6304fb71983d5d8c007126399c
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-31 19:13:47 -0300

    API na Página Register e implementação da Navegação nessa página.
    Implementada a primeira camada de comunicação com a API, na Página Register, com a função "handleRegister";
    Implementado o React Hook "useHistory" para a navegação automática do usuário para a Rota Raiz "/" após o "Alert" de Cadastro.

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,30 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import api from '../../services/api';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
     export default function Register() {
-        return (
-            <div className="register-container">
-                <div className="content">
-                    <section>
-                        <img src={logoImg} alt="Be The Hero"/>
-                            <h1> Cadastre-se </h1>
-                                <p> Faça seu cadastro, entre na plataforma e ajude pesssoas a encontrarem os casos da sua ONG. </p>
-                                    <Link className="back-link" to="/"> <FiArrowLeft size={20} color="#E02041"/> Já sou cadastrado </Link>    
-                    </section>
-                        <form>
-                            <input placeholder="Nome da ONG"/>
-                            <input placeholder="E-Mail" type="email"/>
-                            <input placeholder="WhatsApp"/>
-                                <div className="input-group">
-                                    <input placeholder="Cidade"/>
-                                    <input placeholder="UF" style={{ width: 80 }}/>
-                                </div>
-                                    <button className="button" type="Submit"> Cadastrar </button>
-                        </form>
-                </div>
-            </div>
-        );
+        const [name, setName] = useState('');
+        const [email, setEmail] = useState('');
+        const [whatsapp, setWhatsapp] = useState('');
+        const [city, setCity] = useState('');
+        const [uf, setUf] = useState('');
+
+        const history = useHistory();
+
+            async function handleRegister(event) {
+                event.preventDefault();
+                    const data = { name, email, whatsapp, city, uf };
+                        try {
+                            const response = await api.post('ongs', data);
+                                alert(`Seu ID de acesso: ${response.data.id}`);
+                                    history.push('/');
+                        } catch (error) {
+                            alert('Erro no cadastro, tente novamente.');
+                        }
+            }
+
+                return (
+                    <div className="register-container">
+                        <div className="content">
+                            <section>
+                                <img src={logoImg} alt="Be The Hero"/>
+                                    <h1> Cadastre-se </h1>
+                                        <p> Faça seu cadastro, entre na plataforma e ajude pesssoas a encontrarem os casos da sua ONG. </p>
+                                            <Link className="back-link" to="/"> <FiArrowLeft size={20} color="#E02041"/> Já sou cadastrado </Link>    
+                            </section>
+                                <form onSubmit={handleRegister}>
+                                    <input placeholder="Nome da ONG" value={name} onChange={e => setName(e.target.value)}/>
+                                    <input placeholder="E-Mail" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+                                    <input placeholder="WhatsApp" value={whatsapp} onChange={e => setWhatsapp(e.target.value)}/>
+                                        <div className="input-group">
+                                            <input placeholder="Cidade" value={city} onChange={e => setCity(e.target.value)}/>
+                                            <input placeholder="UF" style={{ width: 80 }} value={uf} onChange={e => setUf(e.target.value)}/>
+                                        </div>
+                                            <button className="button" type="Submit"> Cadastrar </button>
+                                </form>
+                        </div>
+                    </div>
+                );
     }
