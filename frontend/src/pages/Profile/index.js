4e44791130a2f842c1b8a89a005fd3d803db8b29
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-31 21:22:13 -0300

    Logout na Página Profile + Navegação para a Rota Raiz.
    Implementado o LogOut na Página Profile para a ONG "se deslogar", através da nova Função "handleLogout";
    Implementado o React Hook "useHistory" nessa página, onde no "onClick" do botão "FiPower" é feita a navegação automática para a Rota Raiz da Aplicação;

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import api from '../../services/api';
 import './styles.css';
@@ -7,6 +7,7 @@ import logoImg from '../../assets/logo.svg';
 
     export default function Profile() {
         const [incidents, setIncidents] = useState([]);
+        const history = useHistory();
         const ongId = localStorage.getItem('ongId');
         const ongName = localStorage.getItem('ongName');
 
@@ -24,13 +25,18 @@ import logoImg from '../../assets/logo.svg';
                     }
                 }
 
+                function handleLogout() {
+                    localStorage.clear();
+                        history.push('/');
+                }
+
                     return (
                         <div className="profile-container">
                             <header>
                                 <img src={logoImg} alt="Be The Hero"/>
                                     <span> Bem-vinda, {ongName} </span>
                                         <Link className="button" to="/incidents/new"> Cadastrar Caso </Link>
-                                            <button type="button"> <FiPower  size={25} color="#E02041"/> </button>
+                                            <button onClick={handleLogout} type="button"> <FiPower  size={25} color="#E02041"/> </button>
                             </header>
                                 <h1> Casos Cadastrados </h1>
                                     <ul>
