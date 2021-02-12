4c6fa000bb01e1e4a3885c0ad88f90c462851f4c
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-12 10:22:10 -0300

    Nova Splash Screen e Ícone para o App Mobile.
    Configurada as imagens da Splash Screen (tela de carregamento/recarregamento) e do Ícone da Aplicação Mobile;
    Para correção na Splash Screen (que continha parte do fundo/bordas em branco), alteramos o "backgroundColor" no "app.json" para a cor padrão da Aplicação.

--- mobile/App.js
+++ mobile/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello OmniStack</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
