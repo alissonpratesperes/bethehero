0c599b58a98ce793fc840179ca8dbf779813f3ec
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-13 17:11:15 -0300

    Configuradas as Funções dos Botões da Página Detail.
    Instalada nova dependência "expo-mail-composer" para o envio de e-mail da Página Detail pelo próprio Aplicativo Mobile;
    Criada a função "sendMail" para o envio do e-mail, ainda com o conteúdo estático e configurada a chamada dessa função no
    atributo "onPress" do TouchableOpacity da Página Detail;
    Criada a função "sendWhatsapp" para o envio de mensagem, ainda com o conteúdo estático e configurada a chamada dessa função no atributo "onPress" do TouchableOpacity da Página Detail;

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
     export default function Detail() {
         const navigation = useNavigation();
+        const message = 'Olá APAD, estrou em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00';
 
             function navigateBack() {
                 navigation.goBack();
+            }
+            function sendMail() {
+                MailComposer.composeAsync({
+                    subject: 'Herói do caso: Cadelinha atropelada',
+                    recipients: ['a.pratesperes@gmail.com'],
+                    body: message
+                });
+            }
+            function sendWhatsapp() {
+                Linking.openURL(`whatsapp://send?phone=5554991768785&text=${message}`)
             }
 
                 return (
@@ -33,10 +45,10 @@ import styles from './styles';
                             <Text style={styles.heroTitle}> Seja o herói desse caso. </Text>
                             <Text style={styles.heroDescription}> Entre em contato: </Text>
                                 <View style={styles.actions}>
-                                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                                         <Text style={styles.actionText}> WhatsApp </Text>
                                     </TouchableOpacity>
-                                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                                         <Text style={styles.actionText}> E-Mail </Text>
                                     </TouchableOpacity>
                                 </View>
