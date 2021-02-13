c078ca46ae30ce0f6fac18dff1a71b10fd17c680
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-13 16:30:21 -0300

    Navegação "goBack" no "backButton" da Página Detail.
    Implementada a Navegação no "backButton" da Página Detail, utilizando o "UseNavigation" do React Navigation.

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,39 +1,46 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
     export default function Detail() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Image source={logoImg}/>
-                        <TouchableOpacity style={styles.backButton} onPress={() => {}}>
-                            <Feather name="arrow-left" size={25} color="#E02041"/>
-                        </TouchableOpacity>
-                </View>
-                <View style={styles.incident}>
-                    <Text style={[styles.incidentProperty, {marginTop: 0}]}> ONG: </Text>
-                    <Text style={styles.incidentValue}> APAD </Text>
-                    <Text style={styles.incidentProperty}> CASO: </Text>
-                    <Text style={styles.incidentValue}> Cadelinha atropelada. </Text>
-                    <Text style={styles.incidentProperty}> VALOR: </Text>
-                    <Text style={styles.incidentValue}> R$ 120,00 </Text>
-                </View>
-                <View style={styles.contactBox}>
-                    <Text style={styles.heroTitle}> Salve o dia! </Text>
-                    <Text style={styles.heroTitle}> Seja o herói desse caso. </Text>
-                    <Text style={styles.heroDescription}> Entre em contato: </Text>
-                        <View style={styles.actions}>
-                            <TouchableOpacity style={styles.action} onPress={() => {}}>
-                                <Text style={styles.actionText}> WhatsApp </Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={styles.action} onPress={() => {}}>
-                                <Text style={styles.actionText}> E-Mail </Text>
-                            </TouchableOpacity>
+        const navigation = useNavigation();
+
+            function navigateBack() {
+                navigation.goBack();
+            }
+
+                return (
+                    <View style={styles.container}>
+                        <View style={styles.header}>
+                            <Image source={logoImg}/>
+                                <TouchableOpacity style={styles.backButton} onPress={navigateBack}>
+                                    <Feather name="arrow-left" size={25} color="#E02041"/>
+                                </TouchableOpacity>
                         </View>
-                </View>
-            </View>
-        );
+                        <View style={styles.incident}>
+                            <Text style={[styles.incidentProperty, {marginTop: 0}]}> ONG: </Text>
+                            <Text style={styles.incidentValue}> APAD </Text>
+                            <Text style={styles.incidentProperty}> CASO: </Text>
+                            <Text style={styles.incidentValue}> Cadelinha atropelada. </Text>
+                            <Text style={styles.incidentProperty}> VALOR: </Text>
+                            <Text style={styles.incidentValue}> R$ 120,00 </Text>
+                        </View>
+                        <View style={styles.contactBox}>
+                            <Text style={styles.heroTitle}> Salve o dia! </Text>
+                            <Text style={styles.heroTitle}> Seja o herói desse caso. </Text>
+                            <Text style={styles.heroDescription}> Entre em contato: </Text>
+                                <View style={styles.actions}>
+                                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                                        <Text style={styles.actionText}> WhatsApp </Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                                        <Text style={styles.actionText}> E-Mail </Text>
+                                    </TouchableOpacity>
+                                </View>
+                        </View>
+                    </View>
+                );
     }
