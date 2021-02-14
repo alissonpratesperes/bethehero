f18e6d06836bd1b4de5975eedc9abaaca732e085
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-13 23:36:03 -0300

    Página Incidents comunicando com o Back-End.
    Implementada a comunicação com o Back-End na Página Incidents - principal do Aplicativo Mobile;
    Instalada nova dependência "intl" para a formatação do valor numérico para "Valor em Real".

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,6 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import Routes from './src/routes';
 
   export default function App() {
