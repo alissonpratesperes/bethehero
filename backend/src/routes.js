208b689f08edf5399b77d11cf17fc99062e87568
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-15 10:31:15 -0300

    Primeiro contato com MIDDLEWARE e primeiras validações com a Celebrate.
    Conforme primeiras instruções da última aula, iremos implementar as Validações com a Biblioteca Celebrate nas Rotas de Criação e Alteração, visto que não será tão necessário existirem validações em Rotas de Listagem(Index);
    Implementada a validação pelo Segmento do BODY da requisição para cada campo, todos são OBRIGATÓRIOS, e-mail tem que ser no formato de e-mail, whatsapp tem que ter 10 ou 11 dígitos e uf tem que ter 2 letras;
    Implementada a validação pelo Segmento do HEADER da requisição, para este não foi necessário utilizar o ".keys({})" visto que, como numa requisição HTTP podem existir vários HEADERS, a lógica é que talvez não se saiba/conheça todos, portanto o objeto com a validação foi inserido dentro do ".object()" e o ".keys({})" foi substituído pelo ".unknown()", agora conseguimos obter êxito nos testes com e sem o "Authorization"(ID da ONG na Rota "Profile") no HEADER - necessária essa configuração específica, pois tínhamos retorno de erro pelo Insomnia com: Host is not allowed.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,30 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const IncidentController = require('./controllers/IncidentController');
 const OngController = require('./controllers/OngController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
+const { join } = require('./database/connection');
 const routes = express.Router();
 
     routes.get('/incidents', IncidentController.index);
     routes.post('/incidents', IncidentController.create);
     routes.delete('/incidents/:id', IncidentController.delete);
     routes.get('/ongs', OngController.index);
-    routes.post('/ongs', OngController.create); 
-    routes.get('/profile', ProfileController.index); 
+    routes.post('/ongs', celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required().min(10).max(11),
+            city: Joi.string().required(),
+            uf: Joi.string().required().length(2)
+        })
+    }), OngController.create); 
+    routes.get('/profile', celebrate({
+        [Segments.HEADERS]: Joi.object({
+            authorization: Joi.string().required()
+        }).unknown()
+    }), ProfileController.index); 
     routes.post('/sessions', SessionController.create);
     
         module.exports = routes;
