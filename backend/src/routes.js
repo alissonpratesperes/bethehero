81a982ca03cc908384712b0f8277ee01d3ec9ab4
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-15 10:47:39 -0300

    Finalização da implementação das Validações com a Biblioteca JOI.
    Implementada validação com o Segmento dos PARAMS para a Deleção dos Casos com a Celebrate, agora valida se a deleção na Rota Incidents possui o ID do Caso a ser deletado;
    Implementada validação com o Segmento de QUERY para a Paginação, tendo que ser em valor numérico para cada página.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,9 +7,17 @@ const SessionController = require('./controllers/SessionController');
 const { join } = require('./database/connection');
 const routes = express.Router();
 
-    routes.get('/incidents', IncidentController.index);
+    routes.get('/incidents',  celebrate({
+        [Segments.QUERY]: Joi.object().keys({
+            page: Joi.number()
+        })
+    }),IncidentController.index);
     routes.post('/incidents', IncidentController.create);
-    routes.delete('/incidents/:id', IncidentController.delete);
+    routes.delete('/incidents/:id', celebrate({
+        [Segments.PARAMS]: Joi.object().keys({
+            id: Joi.number().required()
+        })
+    }), IncidentController.delete);
     routes.get('/ongs', OngController.index);
     routes.post('/ongs', celebrate({
         [Segments.BODY]: Joi.object().keys({
