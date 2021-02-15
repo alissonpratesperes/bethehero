6fa2ab79ec19c4c42eecbbd50c900ff15e5cabf1
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-15 16:32:08 -0300

    Teste com o JEST no arquivo "generateUniqueId" do CRYPTO.
    Corrigido o Export da Função no arquivo "src/utils/generateUniqueId.js";
    Configurado o arquivo "tests/unit/generateUniqueId.spec.js" para testar a Geração do ID pelo CRYPTO, importando a Função do CRYPTO que foi extraída do "OngController.js" e armazenando na variável/constante "id", posteriormente testando com o "expect" se possui a quantidade(toHaveLength) de 8 caracteres - teste bem-sucedido.

--- backend/tests/unit/generateUniqueId.spec.js
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -1,6 +1,8 @@
+const generateUniqueId = require('../../src/utils/generateUniqueId');
+
 describe('Generate Unique ID', () => {
     it('should generate an unique ID', () => {
-        expect(2 + 2).toBe(4);
-        expect(2 + 2).toBe(5);
+        const id = generateUniqueId();
+            expect(id).toHaveLength(8);
     });
 });
