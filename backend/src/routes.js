5f64b2c79eb20009a400ff5ca95a51f435f959e8
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-17 09:35:12 -0300

    Correção da validação com a CELEBRATE para o campo "whatsapp".
    Corrigido erro cometido anteriormente na Validação com a CELEBRATE, que ao tentar validar o campo "whatsapp" o Insomnia retornava o erro: "\whatsapp\ must be less than or equal to 11", devido ao Joi estar configurado como "number". Foi necessário alterar o Joi para "string()" para podermos utilizar o "min()" e o "max()" e obter a validação correta para este campo.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -31,7 +31,7 @@ const routes = express.Router();
         [Segments.BODY]: Joi.object().keys({
             name: Joi.string().required(),
             email: Joi.string().required().email(),
-            whatsapp: Joi.number().required().min(10).max(11),
+            whatsapp: Joi.string().required().min(10).max(11),
             city: Joi.string().required(),
             uf: Joi.string().required().length(2)
         })
