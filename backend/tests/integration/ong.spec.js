cd17efbdb78f313fbcbbc10cb4c897e2482382a0
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-17 10:08:28 -0300

    Teste com o JEST no arquivo "ong.spec.js" da pasta "integration" para o teste de Cadastro de ONG.
    Criada o arquivo para os Testes de Integração: "tests/integration/ong.spec.js" importando o arquivo "connection" para o Banco de Dados;
    Criado o "it - 'should be able to create a new ONG'" descritivo que executa primeiramente (beforeEach) as Migrations para o DB "src/database/test.sqlite" e segundamente (afterAll) de tudo destrói a conexão com esse DB para não haver nada pendurado - o JEST acusava erros referentes a isso;
    Nesse "it - 'should be able to create a new ONG'" é feito a requisição HTTP - Post para a rota "/ongs" com todos os campos padrão de cadastro, para testarmos o Cadastro de ONG inteiro;
    Nos "expects", esperamos de "response.body" um "id" - que é o ID gerado pelo Crypto no Cadastro e esperamos de "response.body.id" que tenha 8 caracteres - que é o padrão estabelecido no Crypto para essa geração HEX.
    Os testes foram realizados com o JEST depois dessas implementações e obtivemos sucesso com o Script "npm test".

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,8 +1,24 @@
 const request = require('supertest');
 const app = require('../../src/app');
+const connection = require('../../src/database/connection');
 
     describe('ONG', () => {
-        it('should be able to create a new ONG', () => {
+        beforeEach(async () => {
+            await connection.migrate.latest();
+        });
+        afterAll(async () => {
+            await connection.destroy();
+        });
 
+        it('should be able to create a new ONG', async () => {
+            const response = await request(app).post('/ongs').send({
+                name: "ONG",
+                email: "contact@ong.com.br",
+                whatsapp: "54000000000",
+                city: "Ong City",
+                uf: "UF"
+            });
+                expect(response.body).toHaveProperty('id');
+                expect(response.body.id).toHaveLength(8);
         });
     });
