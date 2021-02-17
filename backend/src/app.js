45e39012f037b0ab14278b828bf97b18305d1224
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-17 09:17:35 -0300

    Configuração da Aplicação em duas "Rotas": APP e SERVER.
    Separada a aplicação em duas rotas, para quando os testes estiverem sendo executados, não utilizarem a porta 3333 - criando o arquivo "src/server.js" que contém a porta 3333 para a rota de utilização;
    Renomeado o antigo arquivo "src/index.js" para "src/app.js" e implementado o comando "module.exports" onde exportamos o "app" e importamos dentro do "src/server.js", assim, quando o Script "test" for executado, chamará o arquivo "src/app.js" sem utilizar a porta 3333, e quando o Script "start" for executado, chamará o arquivo "src/server.js" que utilizará a porta 3333 como rota para o arquivo "src/app.js".

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,4 +8,5 @@ const app = express();
     app.use(express.json());
     app.use(routes);
     app.use(errors());
-    app.listen(3333);
+    
+        module.exports = app;
--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,5 +1,8 @@
-describe('ONG', () => {
-    it('should be able to create a new ONG', () => {
-        
+const request = require('supertest');
+const app = require('../../src/app');
+
+    describe('ONG', () => {
+        it('should be able to create a new ONG', () => {
+
+        });
     });
-});
